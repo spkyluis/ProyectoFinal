45ca606740897d3a51f1b2daed1482fb60c7eda1
Luis Altamiranda <la_107@hotmail.com>
2023-03-26 13:51:50 -0300

    prueba de acceso a los datos

--- src/ProyectoFinal/usoPartido.java
+++ src/ProyectoFinal/usoPartido.java
@@ -6,10 +6,24 @@ import java.util.Scanner; // Import the Scanner class to read text files
 public class usoPartido {
   public static void main(String[] args) {
     try {
+    	// variables para obtener los datos
+    	String partidoId;
+    	String equipo1;
+    	String goles1;
+    	String equipo2;
+    	String goles2;
+    
       File resultado = new File("resultadosMod.csv");
       Scanner archivoResultado = new Scanner(resultado);
       while (archivoResultado.hasNextLine()) {
         String Partido = archivoResultado.nextLine();
+        
+        String[] subcadenas = Partido.split(","); // separa según las comas
+        
+        partidoId = subcadenas[0].trim(); // .trim() elimina espacios en blanco
+        equipo1 = subcadenas[1].trim();
+        
+        System.out.println(partidoId + equipo1);
         System.out.println(Partido);
       }
       archivoResultado.close();
