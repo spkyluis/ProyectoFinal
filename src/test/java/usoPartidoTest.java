ac9398ba9e513fa63088871af8a3a054e255ae4c
Luis Altamiranda <la_107@hotmail.com>
2023-04-09 19:53:28 -0300

    testing de cálculo de pronóstico

--- src/test/java/usoPartidoTest.java
+++ src/test/java/usoPartidoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 class usoPartidoTest {
 
+		
 	@Test
 	public void testBuscarIndicePartido() {
 		Partido[] partidos=new Partido[3];
@@ -15,7 +16,21 @@ class usoPartidoTest {
 		
 		assertEquals(1,indice);
 		
-		//fail("Not yet implemented");
+	}
+	@Test
+	public void testCalcularPuntaje() {
+		Partido[] partidos=new Partido[3];
+		partidos[0]=new Partido("1","Equipo 1",0,"Equipo 2",0,1);
+		partidos[1]=new Partido("2","Equipo 3",1,"Equipo 4",0,1);
+		partidos[2]=new Partido("3","Equipo 5",2,"Equipo 6",3,1);
+		
+		Pronostico[] pronosticos=new Pronostico [2];
+		pronosticos[0]=new Pronostico(partidos[0],"Equipo 1",Resultado.empate,"Juan");
+		pronosticos[1]=new Pronostico(partidos[1],"Equipo 3",Resultado.ganador,"Juan");
+		
+		assertEquals(pronosticos[0].puntos(),1);  // acierto empate 
+		assertEquals(pronosticos[1].puntos(),1);  // acierto equipo 3 ganador
+		
 	}
 
 }
